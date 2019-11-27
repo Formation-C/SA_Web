e57877ebdc7e71ca9e5e717bdbbf475f79b0446d
CMLCTRLEMEA - Sylvain Aillaud <sylvain.aillaud@carrier.com>
2019-11-27 15:10:24 +0100

    Init Repository

---
+++ SA_Forum/SA_Forum.cpp
@@ -0,0 +1,13 @@
+// SA_Forum.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello WebSite!\n";
+}
+
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
+// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+
